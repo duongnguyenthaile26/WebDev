eabb77a6f4864cce9a38cb24c0520f088615f743
TonCD <thichkhampha11@gmail.com>
2024-01-10 21:31:02 +0700

    about us page

--- controllers/home.js
+++ controllers/home.js
@@ -45,5 +45,49 @@ async function render(req, res, next) {
     next(error);
   }
 }
+async function About(req, res, next) {
+  try {
+    // cho sidebar
+    const categories = await Category.find({}).select("-__v");
+    const options = categories.map((category) => category.name);
+
+    // nếu người dùng là admin
+    if (req.user && req.user.role === "admin") {
+      return res.render("about", {
+        user: req.user,
+        options,
+      });
+    }
+
+    const cheapestFlags = await Flag.find({})
+      .sort({ price: 1 })
+      .limit(12)
+      .select("-__v");
+
+    const flagsByCategory = [];
+    for (let i = 0; i < options.length; i++) {
+      const category = { name: options[i] };
+      const flags = await Flag.aggregate([
+        { $match: { type: category.name.toLowerCase() } },
+        { $sample: { size: 4 } },
+        { $project: { __v: 0 } },
+      ]);
+      category.flags = flags;
+      flagsByCategory.push(category);
+    }
+
+    // nếu người dùng không phải admin (guest hoặc user)
+    console.log(req.user);
+    res.render("about", {
+      user: req.user,
+      cheapestFlags,
+      flagsByCategory,
+      options,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
 
 exports.render = render;
+exports.About = About;
--- routes/home.js
+++ routes/home.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const controller = require(path.join(__dirname, "..", "controllers", "home"));
 
 router.route("/").get(controller.render);
+router.route("/about").get(controller.About);
 
 module.exports = router;
